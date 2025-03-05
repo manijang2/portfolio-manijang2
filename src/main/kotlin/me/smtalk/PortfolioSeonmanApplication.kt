e649e203f1807f97eb627c04d1f9757ec3396b3f
manijang2 <manijang2@naver.com>
2025-03-05 11:00:03 +0900

    [미션1] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/me/smtalk/PortfolioSeonmanApplication.kt
@@ -0,0 +1,11 @@
+package me.smtalk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioSeonmanApplication
+
+fun main(args: Array<String>) {
+    runApplication<PortfolioSeonmanApplication>(*args)
+}
---
+++ src/test/kotlin/me/smtalk/PortfolioSeonmanApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.smtalk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioSeonmanApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
